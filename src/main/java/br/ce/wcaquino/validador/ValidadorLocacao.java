cbb033d2c66e3bb5bbfdf3f7696be701f16539f1
RafaelFreimuth <rafaelfreimuth@outlook.com>
2022-02-22 20:25:21 -0300

    criando validação de campos obrigatorios para a locação de um gilme

--- src/main/java/br/ce/wcaquino/validador/ValidadorLocacao.java
+++ src/main/java/br/ce/wcaquino/validador/ValidadorLocacao.java
@@ -1,11 +1,23 @@
 package br.ce.wcaquino.validador;
 
 import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.utils.ObjectUtil;
 
 public class ValidadorLocacao {
 
 	public void validarLocacao(Locacao locacao) {
+		validarCamposObrigatorios(locacao);
 		naoPermitirAlugarFilmeSemEstoque(locacao);
+	}
+
+	private void validarCamposObrigatorios(Locacao locacao) {
+		if (ObjectUtil.isNull(locacao.getFilme())) {
+			throw new RuntimeException("O Filme é obrigatório para a locação de um filme.");
+		}
+		
+		if (ObjectUtil.isNull(locacao.getUsuario())) {
+			throw new RuntimeException("O Usuário é obrigatório para a locação de um filme.");
+		}
 	}
 
 	private void naoPermitirAlugarFilmeSemEstoque(Locacao locacao) {
